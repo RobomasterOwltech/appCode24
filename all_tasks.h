771912def85e24c795605dde52183b7077a3d2e1
JorgePerC <jorgeperez_1@live.com>
2024-04-15 14:57:25 -0600

    sample task

---
+++ all_tasks.h
@@ -0,0 +1,21 @@
+#ifndef ALL_TASKS_H
+#define ALL_TASKS_H
+
+/**
+  * Define all the tasks you will use in this file
+  *
+  */
+
+
+
+/**
+  * @brief          sample task
+  * @param[in]      argument: NULL
+  * @retval         none
+  */
+extern void red_led_task(void * argument);
+
+#endif
+
+
+
---
+++ src/red_led_task.c
@@ -0,0 +1,29 @@
+
+#include "all_tasks.h"
+
+#include "cmsis_os.h"
+#include "main.h"
+
+
+/**
+  * @brief          red led task
+  * @param[in]      argument: NULL
+  * @retval         none
+  */
+
+void red_led_task(void * argument)
+{
+
+    while(1)
+    {
+        HAL_GPIO_WritePin(LED_R_GPIO_Port, LED_R_Pin, GPIO_PIN_SET);
+        osDelay(500);
+        HAL_GPIO_WritePin(LED_R_GPIO_Port, LED_R_Pin, GPIO_PIN_RESET);
+        osDelay(500);
+        HAL_GPIO_WritePin(LED_R_GPIO_Port, LED_R_Pin, GPIO_PIN_RESET);
+        osDelay(500);
+
+    }
+}
+
+
